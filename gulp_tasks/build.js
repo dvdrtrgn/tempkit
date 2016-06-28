a3d176af4dbdf82b46dbb92b1c112f05057edad1
dvdrtrgn <dvdrtrgn@gmail.com>
2016-06-27 22:01:54 -0500

    simplify

--- gulp_tasks/build.js
+++ gulp_tasks/build.js
@@ -7,27 +7,17 @@
 
 // Dependencies
 var gulp        = require('gulp');
-var del         = require('del');
 var runSequence = require('run-sequence');
 
 // Task
-gulp.task('clean', function(cb) {
-
-  // Remove build
-  del('./build/*');
+gulp.task('build', function(cb) {
 
   // Run tasks synchronously
   return runSequence(
-    [ 'remove-build' ],
     [ 'assets' ],
     [ 'scripts' ],
     [ 'styles' ],
     [ 'views' ],
     cb
   );
-});
-
-// Remove build
-gulp.task('remove-build', function() {
-  return del('./build/*');
 });
