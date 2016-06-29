05dd138e91ead95d36e98525937804712aea5097
dvdrtrgn <dvdrtrgn@gmail.com>
2016-06-28 22:07:00 -0500

    rough in expander

--- source/scripts/_main.js
+++ source/scripts/_main.js
@@ -16,6 +16,9 @@
   var W = (W && W.window || window),
       C = (W.C || W.console || {});
 
+    $.inlineSvgs();
+    $.watchHash();
+    $.watchWidth();
   // - - - - - - - - - - - - - - - - - -
   // ASSIGN
 
@@ -25,16 +28,72 @@
       },
       El = $.reify({
         body: 'body',
+        content: '.content',
+        choices: '.grid li',
       });
 
   // - - - - - - - - - - - - - - - - - -
   // RUNTIME
 
-  function bind() {
-    $.inlineSvgs();
-    $.watchHash();
-    $.watchWidth();
+  $.fn.targetHeight = function() {
+    var ele = $(this),
+        dat = ele.data();
+    return Number(dat.targetHeight = dat.targetHeight || ele.innerHeight());
+  };
+  $.fn.expand = function (px) {
+    var ele = $(this);
+    ele.css({
+      height: ele.targetHeight() + Number(px || 100),
+    });
+    return this;
+  };
+  $.fn.shrink = function (px) {
+    var ele = $(this);
+    ele.css({
+      height: Number(px || ele.targetHeight()),
+    });
+    return this;
+  };
 
+  function wrapTargets(i, e) {
+    var ele = $(e).addClass('ani'),
+        div;
+
+    div = $('<div>')
+    .addClass('target')
+    .css({ // fill container and save size
+      width: ele.innerWidth(),
+      height: ele.targetHeight(),
+    }).on('click', insertContent);
+
+    div.append(e.innerHTML);
+    ele.empty().append(div);
+  }
+
+  function toggle(par, con) {
+    if (con.is('.shown')) {
+      par.shrink();
+      con.shrink('0').removeClass('shown');
+    } else {
+      par.expand(con.targetHeight());
+      con.expand('0').addClass('shown');
+    }
+  }
+
+  function insertContent(evt) {
+    var me = $(evt.delegateTarget),
+        con = $('.content').first(),
+        par = me.parent();
+
+    if (par.has(con).length === 0) {
+      par.append(con);
+    }
+    toggle(par, con);
+  }
+
+  function bind() {
+    El.content.expand().shrink('0').addClass('ani');
+    El.choices.each(wrapTargets);
   }
 
   // - - - - - - - - - - - - - - - - - -
