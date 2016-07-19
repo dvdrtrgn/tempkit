f7b8dc03cc10df57fdda58f6d09952f4c4d318d6
dvdrtrgn <dvdrtrgn@gmail.com>
2016-07-19 11:04:19 -0500

    move unused functions to lib

--- source/scripts/_main.js
+++ source/scripts/_main.js
@@ -26,35 +26,9 @@ define(['jqxtn', 'lodash'], function ($, _) {
 
   function keyExperiment () {
 
-    function filterKeys(evt, bool) {
-      var keys = {
-        alt: evt.altKey,
-        ctrl: evt.ctrlKey,
-        meta: evt.metaKey,
-        shift: evt.shiftKey,
-      };
-      if (bool){
-        keys.evt = evt;
-      }
-      return keys;
-    }
-
-    $.fn.con = function () { // help OSX assert(control+click !== contextual-click)
-      var me = 'contextmenu.con'; // start as a name
-      function check(evt) {
-        if (!evt.button && evt.ctrlKey) {
-          evt.preventDefault(); // stop contextmenu
-          evt.type = 'click'; // mutate event
-          me.trigger(evt); // recall as click
-        }
-      }
-      me = $(this).off(me).on(me, check); // recycle var & reattach
-      return me.on.apply(this, arguments); // do normal event/listener
-    };
-
-    function readKeys(evt) {
-      C.debug(filterKeys(evt));
-    }
+    // function readKeys(evt) {
+    //   C.debug($.filterKeys(evt));
+    // }
     // end keyExperiment
   }
 
