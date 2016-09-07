caf3a13e4cdb8fc7a0790aee6800e0ae89029ccb
dvdrtrgn <dvdrtrgn@gmail.com>
2016-09-07 16:13:39 -0500

    make independent config

---
+++ source/scripts/_main-push.js
@@ -0,0 +1,79 @@
+/*jslint white:false */
+/*global define, window, jQuery */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ rev. 2016-08 dvdrtrgn
+ USE: bootstrap
+ */
+require.config({
+  baseUrl: 'scripts',
+  paths: {
+    jquery: '../vendors/jquery/jquery',
+    jqxtn: './libs/jq-xtn',
+  },
+});
+(function (factory) {
+  'use strict';
+  if (!(typeof define === 'function' && define.amd)) {
+    console.warn('shim:_main-push.js');
+    window.Main = factory(jQuery);
+  } else {
+    console.info('AMD:_main-push.js');
+    define(['jqxtn'], factory);
+  }
+}(function ($) {
+  'use strict';
+
+  var W = (W && W.window || window);
+  var C = (W.C || W.console || {});
+
+  $.inlineSvgs();
+  $.watchHash();
+  $.watchWidth();
+  $.watchInputDevice();
+  // - - - - - - - - - - - - - - - - - -
+  // ASSIGN
+
+  var Nom = 'Main',
+    Api = {
+      slick: null,
+    },
+    El = $.reify({
+      body: 'body',
+    });
+
+  // - - - - - - - - - - - - - - - - - -
+  // RUNTIME
+
+
+  // - - - - - - - - - - - - - - - - - -
+  // INIT
+
+  function bind() {
+
+    require(['libs/jq-pusher'], function () {
+      var picker = $('a.preview').hide();
+
+      W._push = picker.pusher();
+    });
+  }
+
+  $.extend(Api, {
+    _el: El,
+  });
+
+  W.setTimeout(W._shim ? shim : bind, 99);
+
+  if (W._dbug > 0) { // Expose
+    W[Nom] = Api;
+    C.warn(Nom, 'exposed', Api);
+  } else {
+    C.debug(Nom, 'loaded', Api);
+  }
+
+  return Api;
+}));
+/*
+
+
+
+ */
