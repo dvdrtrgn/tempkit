c33f72fce02fb89e101281d9045f9bb8c919ab4d
dvdrtrgn <dvdrtrgn@gmail.com>
2016-10-03 13:30:06 -0500

    use eelliotts
    npm install -g babel-cli tape faucet browserify browser-run
    npm install --save-dev babel-preset-es2015 babelify tape

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 var test = require('tape');
+//import test from 'tape';
 
-test('My first test', function (assert) {
-  assert.equal(1, 2, 'Numbers 1 and 2 are the same');
+test('Failing test', (...assert) => {
+  assert[0].equal(1, 2, 'Numbers 1 and 2 are the same');
+  assert[0].end();
+});
+
+test('Passing test', (assert) => {
+  assert.equal(2, 2, 'Numbers 2 and 2 are the same');
   assert.end();
 });
