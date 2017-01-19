d74462aa697d986c592fa1db99f4d2031bf02a2c
dvdrtrgn <dvdrtrgn@gmail.com>
2017-01-19 15:25:17 -0600

    update tasks

--- gulp_tasks/clean.js
+++ gulp_tasks/clean.js
@@ -12,7 +12,7 @@ var del         = require('del');
 // Task
 gulp.task('clean', function(cb) {
   // Beware: The glob pattern ** matches all children and the parent.
-  del(['./.sass-cache/*', './build/*/*']).then(paths => {
-    console.log('Deleted files and folders:'/*, '\n', paths*/);
+  del(['./.sass-cache/*', './build/**', '!./build']).then(paths => {
+    console.log('Deleted files and folders:', '\n', paths);
   }).then(cb);
 });
